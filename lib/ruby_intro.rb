a01e4fef244dc0029b2b275139a97220e1aa7b3a
zachandfox <zachandfox@gmail.com>
2018-01-18 11:33:48 -0600

    Initial setup, and first two parts of first HW

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,43 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.inject(0,:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return arr.max(2).inject(0,:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return arr.combination(2).map{|x,y| x+y}.include? n
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if (/[[:alpha:]]/ =~ s.downcase) == 0
+    return !((/[aeiou]/ =~ s.downcase) == 0)
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s == ''
+    return false
+  elsif s.gsub(/[01]/,'') == ''
+    return s.to_i(2)%4 == 0
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
 end
